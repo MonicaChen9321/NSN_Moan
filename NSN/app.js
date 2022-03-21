e485bef7d6683e61a700a4112706313e42692c8f
monica chen <chenhong@seas.upenn.edu>
2022-03-21 20:18:26 +0800

    initilization monica

---
+++ NSN/app.js
@@ -0,0 +1,30 @@
+// app.js
+App({
+  onLaunch() {
+
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login,
+     wx.getUserInfo,
+     wx.requestPayment)
+
+    wx.BaaS.init('080e914aef975d22fce7')
+    wx.BaaS.auth.loginWithWechat() // 静默登录
+
+    
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
